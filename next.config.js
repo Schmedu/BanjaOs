88264ae18fb056b332734f8e4830889ea0a85a15
Eduard Uffelmann <edu3@gmx.net>
2022-12-02 15:49:02 +0100

    used alternative way to display html [lang] attribute

--- next.config.js
+++ next.config.js
@@ -7,10 +7,3 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 module.exports = withBundleAnalyzer({})
-
-module.exports = {
-  i18n: {
-    locales: ["de"],
-    defaultLocale: "de",
-  },
-};
